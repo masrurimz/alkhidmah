15faec7fc564bea246f7ea2464585c398aa30c2f
Muhammad Zahid Masruri <zahid@hellonimbly.com>
2022-12-29 22:08:36 +0700

    Reconfigure apps to integrate mode

--- apps/maktab/jest.config.ts
+++ apps/maktab/jest.config.ts
@@ -7,5 +7,5 @@ export default {
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/maktab',
+  coverageDirectory: '../../coverage/apps/maktab',
 };
